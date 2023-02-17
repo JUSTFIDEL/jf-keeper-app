1659e293545c410dba8391d2653cd878f63b1d79
JUSTFIDEL <fidenki1429@gmail.com>
2023-02-17 14:10:07 +0100

    a final version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
+	const [notes, setNotes] = useState([]);
+
+	function addNote(note) {
+		setNotes((prevNotes) => {
+			return [...prevNotes, note];
+		});
+	}
+
 	return (
 		<div>
 			<Header />
-			{notes.map((newNotes) => (
-				<Note
-					key={newNotes.key}
-					title={newNotes.title}
-					content={newNotes.content}
-				/>
-			))}
+			<CreateArea onAdd={addNote} />
+			{notes.map((noteItem) => {
+				return (
+					<Note title={noteItem.title} content={noteItem.content} />
+				);
+			})}
 			<Footer />
 		</div>
 	);
